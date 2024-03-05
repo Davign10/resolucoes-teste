05fbfa8359197b2f79b76b46d8f4c261f5d9aafb
Davi Gusmão <davigusmao10@bol.com.br>
2024-03-05 16:50:26 -0300

    Resolvendo exercicios de logica

---
+++ ex003.py
@@ -0,0 +1,15 @@
+''' 3) Descubra a lógica e complete o próximo elemento:
+
+a) 1, 3, 5, 7, ___  9    (soma de 2 em 2)
+
+b) 2, 4, 8, 16, 32, 64, ____  128   (o dobro do anterior)
+
+c) 0, 1, 4, 9, 16, 25, 36, ____  49  (aqui temos uma soma que vai de 1, 3, 5, 7, 9, 11, portanto 36 + 13)
+
+d) 4, 16, 36, 64, ____  100 (aqui temos 2 ao quadrado, 4 ao quadrado... portanto 10 ao quadrado resulta no 100)
+
+e) 1, 1, 2, 3, 5, 8, ____  13 (soma dos 2 números dá o próximo)
+
+f) 2, 10, 12, 16, 17, 18, 19, ____  20   '''
+
+
---
+++ ex004.py
@@ -0,0 +1,7 @@
+'''Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+
+Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?'''
+
+'''Teria que ligar um interruptor, e acender o outro, aguardar e apagar esse segundo. Ao ir
+a sala saberia que a lampada acesa é daquele primeiro interruptor, e a segunda lampada seria a da sala que estivesse escuro mas com a lampada um pouco quente, e a ultima estaria apagada e fria.
+'''
